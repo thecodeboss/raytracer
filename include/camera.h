e05d48ae10d70c4e93e1d97158c8da037f6304e6
Michael Oliver <mikemboliver@gmail.com>
2018-11-23 23:11:16 -0800

    Set up ray generation

--- include/camera.h
+++ include/camera.h
@@ -1,10 +1,12 @@
 #pragma once
 #include "math/matrix.h"
 #include "math/vector.h"
+#include <cmath>
 
 class Camera {
 public:
   Matrix44f camera_to_world;
+  double fov = M_PI_4;
 
   Camera();
   Camera(const Vec3f &position);
--- src/raytracer.cpp
+++ src/raytracer.cpp
@@ -2,38 +2,32 @@
 #include "image.h"
 #include "math/matrix.h"
 #include "math/vector.h"
+#include "ray.h"
 #include <cinttypes>
+#include <cmath>
 #include <iostream>
 
 int main(int argc, char const *argv[]) {
   (void)argc;
   (void)argv;
-  // clang-format off
-  Matrix44f test_matrix(
-    1.1, 1.2, 1.3, 1.4,
-    2.1, 2.2, 2.3, 2.4,
-    3.1, 3.2, 3.3, 3.4,
-    4.1, 4.2, 4.3,4.4
-  );
-  // clang-format on
-  std::cout << test_matrix << std::endl;
-
-  Vec3f test_vector(1, 2, 3);
-  std::cout << test_vector << std::endl;
-  std::cout << test_vector + test_vector << std::endl;
-  std::cout << test_vector.dot(test_vector) << std::endl;
-  std::cout << test_vector.cross(test_vector) << std::endl;
-  std::cout << test_matrix.multiply_vec(test_vector) << std::endl;
 
   Camera camera(Vec3f(-5.0, 2.0, 0.0));
   camera.look_at(Vec3f(0.0, 0.0, 0.0));
 
   Image image(600, 400);
+  double scale = tan(camera.fov / 2);
+  double aspect_ratio = double(image.x) / image.y;
+
   for (int i = 0; i < image.y; i++) {
     for (int j = 0; j < image.x; j++) {
-      float r = float(j) / float(image.x);
-      float g = float(i) / float(image.y);
-      float b = 0.25;
+      double x = (2 * (j + 0.5) / double(image.x) - 1) * aspect_ratio * scale;
+      double y = (1 - 2 * (i + 0.5) / double(image.y)) * scale;
+      Vec3f direction = camera.camera_to_world.multiply_dir(Vec3f(x, y, -1));
+      Ray ray(camera.position(), direction);
+
+      double r = double(j) / double(image.x);
+      double g = double(i) / double(image.y);
+      double b = 0.25;
       uint8_t ir = uint8_t(255.99 * r);
       uint8_t ig = uint8_t(255.99 * g);
       uint8_t ib = uint8_t(255.99 * b);
